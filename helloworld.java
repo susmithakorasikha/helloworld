111704ba067ac3298e554041ab9a0fec39d64c4d
susmithakorasikha <susmithadevikorasikha088@gmail.com>
2025-05-27 15:34:20 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,10 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World");
     }
 }
+
+
+SET PATH=%PATH%;C:\Program Files (x86)\Java\jdk1.6.0_05\bin
